5b8ea74e730c8a66cedaefc5bbd952250a2eb95f
JRFelix01 <85184297+JRFelix01@users.noreply.github.com>
2024-05-15 20:14:55 -0400

    deuxieme commit

--- public_html/view/home/index.php
+++ public_html/view/home/index.php
@@ -2,45 +2,132 @@
 include_once("header.php");
 ?>
 
-<div class="dashboard-content px-3 pt-4 text-justify">
-    <div class="jumbotron">
-        <h2 class="display-6">Présentation de l'Ecole Normale Supérieure</h2>
-        <p class="lead">L'Ecole Normale Supérieure (ENS) est une entité de l'Université d'Etat d'Haïti qui a pour vocation de former des universitaires de haut niveau, des enseignants, des chercheurs et des professeurs destinés à l’enseignement secondaire.
-            Elle offre des formations de premier et de deuxième cycles dans différents domaines. Elle comprend actuellement les sept (7) <a href='<?=APP_DOMAIN . "home/departments"?>'>départements</a> qui suivent: Lettres modernes, Philosophie, Sciences Sociales, Langues vivantes, Mathématiques, Physique, Sciences Naturelles.
-            Les formations de deuxième cycle sont offertes en partenariat avec des Universités étrangères comme Paris8, l'Université des Antilles, l'Université de Poitiers, l'Université de Lyon, l'Université d'Orléans, l'Université de Liège ou l'Université de Montréal, etc.
-        </p>
-        <p class="lead">
-            Les étudiants sont recrutés par un concours, qui a lieu chaque année, en principe, à la rentrée de septembre. La durée des formations du premier cycle est de quatre (4) ans.
-            La première année est l'année préparatoire: elle sert de transition entre l'enseignement secondaire et l'enseignement supérieur. La deuxième, troisième et quatrième années préparent aux différents certificats d'Etudes Supérieures et au diplôme de fin d'études.
-            Ces documents confèrent aux anciens élèves de l'Ecole Normale Supérieure qui les détiennent, le droit d'être nommés dans une chaire de leur spécialité, professeurs titulaires de l'enseignement secondaire.
-            Ils permettent également aux étudiants de poursuivre leur formation en deuxième cycle dans les domaines comme la Philosophie, la Littérature, les Mathématiques, la Physique, la Chimie et en Français Langue Etrangère (FLE).
+<div class="container-fluid">
 
-            La durée de ces formations de deuxième cycle est de deux (2) ans. Les diplômes de deuxième cycle délivrés permettent aux étudiants de continuer en doctorat ou d'intégrer facilement le marché du travail.
-        </p>
-        <hr class="my-4">
+    <div class="row coul-bleu-pale25  ">
+
+        <div class="col-lg-2 col-md-1 ">
+
+        </div>
+
+        <div class="col-lg-8 col-md-10">
+
+
+            <div class="container card-body">
+                <div class="w-100 mx-auto">
+                    <img class="img-fluid" src="../../img/ima2.jpg" alt="oups">
+                </div>
+
+            </div>
+
+        </div>
+        <div class="col-lg-2 col-md-1">
+
+        </div>
     </div>
 
-    <div class="jumbotron">
-        <h2 class="display-6">Historicité de ENS</h2>
-        <p class="lead">
-            L'Ecole Normale Supérieure, inaugurée le 31 octobre 1947, a été créée par la Loi du 28 Juillet 1947 en vue de la formation et du recrutement des professeurs de l'enseignement secondaire et de l'enseignement supérieur des Lettres et des Sciences.
-            Par la loi du 23 janvier 1969, elle était devenue Faculté des Lettres et de Pédagogie qui avait pour but de dispenser un enseignement théorique et pratique. Le 9 octobre 1973, la Faculté des Lettres est redevenue Ecole Normale Supérieure jusqu'aujourd'hui.
-        </p>
-        <hr class="my-4">
+    <div class="row">
+
+        <div class="col-lg-10 col-md-9">
+
+
+
+            <div class=" dashboard-content px-3 pt-4 coul-bleu-pale15 border-5 ">
+                <div class="jumbotron">
+                    <h2 class="display-6 text-center">Présentation de l'Ecole Normale Supérieure</h2>
+                    <p class="lead">L'Ecole Normale Supérieure (ENS) est une entité de l'Université d'Etat d'Haïti qui a
+                        pour vocation de former des universitaires de haut niveau, des enseignants, des chercheurs et
+                        des professeurs destinés à l’enseignement secondaire.
+                        Elle offre des formations de premier et de deuxième cycles dans différents domaines. Elle
+                        comprend actuellement les sept (7) <a href='<?=APP_DOMAIN . "home/departments"?>'>départements</a> qui suivent:
+                        </p>
+                        
+                        <ul class="list-unstyled lead">
+                            <li class="list-inline-item"> Lettres modernes </li>
+                            <li class="list-inline-item"> Philosophie </li>
+                            <li class="list-inline-item"> Sciences sociales </li>
+                            <li class="list-inline-item">langues vivantes </li>
+                            <li class="list-inline-item"> Mathématiques </li>
+                            <li class="list-inline-item"> Physique </li>
+                            <li class="list-inline-item">Sciences naturelles </li>
+                        </ul>
+                        <p class="lead"> 
+                        Les formations de deuxième cycle sont offertes en partenariat avec des Universités étrangères
+                        comme Paris8, l'Université des Antilles, l'Université de Poitiers, l'Université de Lyon,
+                        l'Université d'Orléans, l'Université de Liège ou l'Université de Montréal, etc.
+                    </p>
+                    <p class="lead">
+                        Les étudiants sont recrutés par un concours, qui a lieu chaque année, en principe, à la rentrée
+                        de septembre. La durée des formations du premier cycle est de quatre (4) ans.
+                        La première année est l'année préparatoire: elle sert de transition entre l'enseignement
+                        secondaire et l'enseignement supérieur. La deuxième, troisième et quatrième années préparent aux
+                        différents certificats d'Etudes Supérieures et au diplôme de fin d'études.
+                        Ces documents confèrent aux anciens élèves de l'Ecole Normale Supérieure qui les détiennent, le
+                        droit d'être nommés dans une chaire de leur spécialité, professeurs titulaires de l'enseignement
+                        secondaire.
+                        Ils permettent également aux étudiants de poursuivre leur formation en deuxième cycle dans les
+                        domaines comme la Philosophie, la Littérature, les Mathématiques, la Physique, la Chimie et en
+                        Français Langue Etrangère (FLE).
+
+                        La durée de ces formations de deuxième cycle est de deux (2) ans. Les diplômes de deuxième cycle
+                        délivrés permettent aux étudiants de continuer en doctorat ou d'intégrer facilement le marché du
+                        travail.
+                    </p>
+                    <hr class="my-4">
+                </div>
+                <div class="jumbotron">
+                    <h2 class="display-6 text-center">Historicité de ENS</h2>
+                    <p class="lead">
+                        L'Ecole Normale Supérieure, inaugurée le 31 octobre 1947, a été créée par la Loi du 28 Juillet
+                        1947 en vue de la formation et du recrutement des professeurs de l'enseignement secondaire et de
+                        l'enseignement supérieur des Lettres et des Sciences.
+                        Par la loi du 23 janvier 1969, elle était devenue Faculté des Lettres et de Pédagogie qui avait
+                        pour but de dispenser un enseignement théorique et pratique. Le 9 octobre 1973, la Faculté des
+                        Lettres est redevenue Ecole Normale Supérieure jusqu'aujourd'hui.
+                    </p>
+                    <hr class="my-4">
+                </div>
+
+                <div class="jumbotron">
+                    <h2 class="display-6 text-center">Vision de ENS</h2>
+                    <p class="lead">
+                        L'ENS a pour vision de:
+                    <ul class="lead">
+                        <li> former des professeurs pour l'enseignement secondaire afin de résoudre le problème de
+                            carence de professeurs qualifiés rencontré dans les différentes écoles au niveau national ;
+                        <li> former des personnels qualifiés pour aborder les problèmes socio-politiques et économiques
+                            d'Haïti ;
+                        <li> former des cadres pour l'enseignement supérieur et la recherche afin de proposer des
+                            solutions aux problèmes environnementaux et énergétiques que confronte le monde actuel.
+                    </ul>
+                    </p>
+                    <hr class="my-4">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-lg-2 col-md-3 border-left border-radius-3 px-2 py-2 coul-bleu-pale25 border-start border-5 border-secondary rounded-5 border-opacity-50 ">
+
+            <p class="px-2 border border-danger border-4 mt-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit
+                anim id est laborum.</p>
+
+        </div>
+
+
     </div>
 
-    <div class="jumbotron">
-        <h2 class="display-6">Vision de ENS</h2>
-        <p class="lead">
-            L'ENS a pour vision de:
-        <ul class="lead">
-            <li> former des professeurs pour l'enseignement secondaire afin de résoudre le problème de carence de professeurs qualifiés rencontré dans les différentes écoles au niveau national ;
-            <li> former des personnels qualifiés pour aborder les problèmes socio-politiques et économiques d'Haïti ;
-            <li> former des cadres pour l'enseignement supérieur et la recherche afin de proposer des solutions aux problèmes environnementaux et énergétiques que confronte le monde actuel.
-        </ul>
-        </p>
-        <hr class="my-4">
+    <div class="row">
+
     </div>
+
+
 </div>
 
 <?php
